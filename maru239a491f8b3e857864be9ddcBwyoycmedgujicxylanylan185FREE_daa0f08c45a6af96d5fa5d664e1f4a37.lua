bf3bdb2652098da03ddfafc0e589d4463d56a072
hoangGM <dgekehr@gmail.com>
2026-01-03 20:30:04 +0700

    Update maru239a491f8b3e857864be9ddcBwyoycmedgujicxylanylan185FREE_daa0f08c45a6af96d5fa5d664e1f4a37.lua

--- maru239a491f8b3e857864be9ddcBwyoycmedgujicxylanylan185FREE_daa0f08c45a6af96d5fa5d664e1f4a37.lua
+++ maru239a491f8b3e857864be9ddcBwyoycmedgujicxylanylan185FREE_daa0f08c45a6af96d5fa5d664e1f4a37.lua
@@ -1,11 +1,4 @@
--- Hàm load Maru Hub (chỉ cần gọi một lần)
-function LoadMaruHub()
-    getgenv().Key = "MARU-5MQIF-3T2E-LVLG-RBRSY-7Q4HH"
-    getgenv().id = "1224185899034742906"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
-    
-    -- Delayed kick sau 1,7 phút (67 giây) khi script được kích hoạt
-    spawn(function()
+ spawn(function()
         wait(67)  -- Đợi đúng 67s
         game:GetService("Players").LocalPlayer:Kick(
             "You have been banned from this experience\n\n" ..
@@ -14,7 +7,3 @@ function LoadMaruHub()
             "(Error Code: 600)"
         )
     end)
-end
-
--- Gọi hàm ngay để load hub và bắt đầu timer kick
-LoadMaruHub()
